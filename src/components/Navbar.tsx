c3c6b56fbcf375561a01c7b14902ea6b53debe45
dhanushetty24 <dhanushetty248@gmail.com>
2024-09-20 15:41:02 +0530

    nav bar added

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">Dhanush Shetty</Link>
+      </div>
+      <ul className={styles.navLinks}>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
