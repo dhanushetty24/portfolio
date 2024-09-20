795bf5ec7d079372b1ab994450c51138d5b9e5f4
dhanushetty24 <dhanushetty248@gmail.com>
2024-09-20 15:20:06 +0530

    Boilerplate code  added

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+const Home: React.FC = () => {
+  return (
+    <>
+      <main className="container">
+        <section style={{ textAlign: 'center', padding: '2rem' }}>
+          <h1>Hey, I'm Dhanush Shetty</h1>
+          <p></p>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Home;
